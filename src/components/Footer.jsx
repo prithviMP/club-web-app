a2bfc251ab1f8449cba992e3ed6331d6c7f11280
prithviraj m pillai <prithvi_mp@yahoo.in>
2025-03-18 05:01:42 +0000

    Restored to '3ee36f3baaf814f25eb89b90aeacc0781253d15b'
    Replit-Restored-To: 3ee36f3baaf814f25eb89b90aeacc0781253d15b

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-8 relative bottom-0 w-full z-10">
+      {/* Add your footer content here */}
+    </footer>
+  );
+};
